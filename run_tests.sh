fc784694cb0cc421273ab712510452a927778089
Eugene Darashkevich <evgenedar@mail.ru>
2024-12-17 18:52:58 +0300

    Test many patches

--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,5 @@
 cd src
-PYTHONPATH=. pytest -s tests.py --maxfail=1
-# PYTHONPATH=. pytest tests.py --maxfail=1
+# PYTHONPATH=. pytest -s tests.py --maxfail=1
+PYTHONPATH=. pytest tests.py --maxfail=1
+
+# bash run_tests.sh > output.log 2>&1
--- src/tests.py
+++ src/tests.py
@@ -48,3 +48,34 @@ def test_nodes_rw(nodes_count):
     assert response == {'key': 'a', 'key2': 'b', 'value': 2}
 
     stop_all_nodes(nodes)
+
+
+@pytest.mark.parametrize("patches_count,nodes_count", [
+    (10, 3), (10, 10),
+    (100, 3), (100, 10),
+    (1000, 3), (1000, 10),
+])
+def test_many_patches(patches_count, nodes_count):
+    cluster_info.init_cluster_nodes(nodes_count)
+    nodes = create_nodes()
+    nodes_addresses = cluster_info.get_all_addresses()
+
+    client = Client()
+    
+    all_patches = [{f'key_{i}': f'value_{i}'} for i in range(patches_count)]
+
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        for patch in all_patches:
+            executor.submit(client.send_patch_request, random.choice(nodes_addresses), patch)
+
+    wait_for_broadcast_time = max(1, patches_count * nodes_count / 100) * BROADCAST_TIME
+    time.sleep(wait_for_broadcast_time)
+
+    final_json = {f'key_{i}': f'value_{i}' for i in range(patches_count)}
+
+    for node in nodes:
+        success, response = client.send_get_request(cluster_info.get_node_address(node.id))
+        assert success
+        assert response == final_json
+
+    stop_all_nodes(nodes)
